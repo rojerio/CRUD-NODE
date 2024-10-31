68215d5e8b9cc97751db1a5a87cb5ca688fa2fcd
rojerio <renan_jussiani@estudante.sesisenai.org.br>
2024-10-31 12:37:28 -0300

    ajuste nn finalizado BACKEND

---
+++ modePstgresql/db.js
@@ -0,0 +1,29 @@
+async function connect(){
+    const {Pool} = require("pg") // Pool: CONEXÃO com cache.
+    const pool = new Pool({
+        //connectionString: texto que contém informações do banco de dados (db).
+        connectionString: process.env.CONNECT_STRING
+    });
+
+    const client = await pool.connect(); // await: pausar a execução.
+    console.log('Criou o pool de conexão');
+    
+    // res: criará uma resposta para o cliente // query: ele consulta uma consulta (select now()) do cliente no momento.
+    const res = await client.query('select now()'); 
+    console.log(res.rows[0])
+    client.release(); // o sistema entrega o que está com os requisitos predeficinidos.
+
+    global.connection = pool;
+    return pool.connect();
+}
+connect();
+
+async function selectCustomers() {
+    const client = await connect();
+    const res = await client.query("SELECT * FROM clientes");
+    return res.rows;
+}
+
+module.exports = {
+    selectCustomers
+}
--- modePstgresql/index.js
+++ modePstgresql/index.js
@@ -4,12 +4,19 @@ const port = process.env.PORT; //que esteja na variavel de ambiente PORT de onte
 
 const express = require("express"); // chamar, requerer o Express para um projeto.
 
+const db = require("./db.js")
+
 const app = express() //cria um aplicativo web atraves do Express.
 
 app.get("/", (req,res) => {
     res.json({ // enviar resposta do JSON.
         menssage: "Funcionando!"
     })
+})
+
+app.get("/clientes", async(req,res) => {
+    const clientes = await db.selectCustomers();
+    res.json(clientes);
 })
 
 app.listen(port) //escutar as requisições que vem da porta.
